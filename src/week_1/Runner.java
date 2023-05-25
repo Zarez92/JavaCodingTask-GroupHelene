a20406ab95c2209ffdd3cb7edd6db52fdcba43c7
Zarko_Nikolic <zare.92@hotmail.com>
2023-05-24 21:18:14 -0400

    Updated Runner & Created Zarko_ConsecutiveNumbers task

--- src/week_1/Runner.java
+++ src/week_1/Runner.java
@@ -1,7 +1,11 @@
 package week_1;
 
+import week_2.Zarko_ConsecutiveNumbers;
+
 import static week_1.DivisionWithoutOperator.division;
 import static week_1.Fin_Ra.FIN_RA;
+import static week_1.OddOrEven.ISOddOrEven;
+import static week_2.Zarko_ConsecutiveNumbers.ConsecutiveNumbers;
 
 public class Runner {
 
@@ -12,10 +16,22 @@ public class Runner {
 
     public static void main(String[] args) {
 
+/**
+ * Week_1
+ */
 
         FIN_RA(1);
         System.out.println("-----------------------------------------");
         division(51,6);
+        ISOddOrEven(5);
+
+        System.out.println("-----------------------------------------");
+
+/**
+ * Week_2
+ */
+        ConsecutiveNumbers();
+
 
 
     }
