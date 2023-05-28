495f380ac2dd693025f03bc60f93367fc0d0136a
AzizaA <aakramova88@gmail.com>
2023-05-27 23:38:37 -0400

    swap variables

---
+++ src/week_2/SwapVariables.java
@@ -0,0 +1,17 @@
+package week_2;
+
+public class SwapVariables {
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        System.out.println("X is "+x+", Y is "+y);
+
+        x=x^y;
+        y=x^y;
+        x=x^y;
+        System.out.println("X is "+x+", Y is "+y);
+
+
+
+    }
+}
