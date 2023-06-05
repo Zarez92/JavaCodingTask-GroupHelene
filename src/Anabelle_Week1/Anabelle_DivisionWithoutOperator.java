2c3a6cf5ab049dcfc43b78222c0f86ccc72cd489
anabellemcafee <anabellemcafee@gmail.com>
2023-06-05 12:16:55 -0400

    Updated Division without Operator task

--- src/Anabelle_Week1/Anabelle_DivisionWithoutOperator.java
+++ src/Anabelle_Week1/Anabelle_DivisionWithoutOperator.java
@@ -1,8 +1,35 @@
 package Anabelle_Week1;
 
+import javax.lang.model.SourceVersion;
+import java.io.FilterOutputStream;
+
 public class Anabelle_DivisionWithoutOperator {
-    // ....
+    public static void divide(int num1, int num2) {
+
+        if (num1 == 0 || num2 == 0){
+            System.err.println("Invalid number");
+            System.exit(1);
+        }
+
+        int count = 0;
+        while (num1 >= num2) {
+            num1 = num1 - num2;
+            count++;
+        }
+            int result = count;
+
+        System.out.println("result = " + result);
 
 
-    //   ....
+    }
+
+    public static void main(String[] args) {
+        divide(50,2);
+    }
 }
+
+/*
+2️⃣  Numbers -- Divide without / operator
+        Write a method that can divide two numbers without using division operator.
+
+ */
