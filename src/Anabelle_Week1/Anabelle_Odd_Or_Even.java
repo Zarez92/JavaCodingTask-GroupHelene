886858f1f22484db5e53ec69b83d9db83ae3a37d
anabellemcafee <anabellemcafee@gmail.com>
2023-06-05 11:43:24 -0400

    Created Even or Odd Task

---
+++ src/Anabelle_Week1/Anabelle_Odd_Or_Even.java
@@ -0,0 +1,27 @@
+package Anabelle_Week1;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class Anabelle_Odd_Or_Even {
+
+    public static void main(String[] args) {
+
+        public static void isEvenOrOdd(int num){
+            if (num % 2 == 0){
+                System.out.println(num + " Even");
+            } else {
+                System.out.println(num + " Odd");
+            }
+        }
+
+
+    }
+
+   /*
+   1️⃣  Numbers -- odd & even
+Write a method that can identify  a given number is even or odd.
+EX:
+       identify(5) --> "Odd"
+       identify(6) --> "Even"
+    */
+}
