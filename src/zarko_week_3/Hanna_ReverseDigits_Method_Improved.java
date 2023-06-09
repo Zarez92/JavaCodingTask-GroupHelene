4d81beb7a8ef6edbc9f5c55185ba28ddcf56f8b8
Zarko_Nikolic <zare.92@hotmail.com>
2023-06-09 13:06:48 -0400

    Updated Hanna's code

---
+++ src/zarko_week_3/Hanna_ReverseDigits_Method_Improved.java
@@ -0,0 +1,38 @@
+package zarko_week_3;
+
+
+public class Hanna_ReverseDigits_Method_Improved {
+
+    public static int reverseDigits(int num) {
+        String num1 = Integer.toString(num);
+        String result = "-";
+
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            result += num1.charAt(i);
+        }
+
+        if (num < 0) {
+            System.out.println(result.substring(0, result.length() - 1));
+        } else {
+            System.out.println(result);
+        }
+
+        return (num);
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        reverseDigits(-1000);
+
+    }
+
+
+}
+
+
+
+
+
