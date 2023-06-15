360d81521ea2c1b9fbb7546cc430a256c27f5cc6
Arife <eve.steel@cydeo.com.github.com>
2023-06-15 12:13:04 +0300

    Solutions of week3

--- src/week_3/Eve_ReverseNegativeNumber.java
+++ src/week_3/Eve_ReverseNegativeNumber.java
@@ -5,5 +5,15 @@ public class Eve_ReverseNegativeNumber {
     Input: -35  output: -53
 
      */
+    public static int reverseNum(int  num) {
+        String str = new StringBuilder(""+num).reverse().toString();
+        if(num < 0) {
+            str = "-"+str.substring(0, str.length()-1);
+        }
+        return Integer.valueOf(str);
+    }
 
+    public static void main(String[] args) {
+        System.out.println(reverseNum(-10));
+    }
 }
